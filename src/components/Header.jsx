ce4fbe35a1fb2deaaf7517c9c10c009ec6c75aa3
danielmoessner <github@moesibert.de>
2021-06-09 13:29:06 +0200

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Component({ title, subtitle, sideImage }) {
         <div className="relative overflow-hidden">
           <Container>
             <div className="flex flex-col-reverse md:flex-row md:justify-between md:items-center">
-              <div className="max-w-xl py-8 md:pt-12 md:pb-16">
+              <div className="max-w-xl py-8 md:pt-8 md:pb-10">
                 <h1 className="text-4xl font-medium text-teal-050 md:text-5xl sm:tracking-tight">
                   {title}
                 </h1>
@@ -19,7 +19,7 @@ function Component({ title, subtitle, sideImage }) {
               </div>
               {sideImage && (
                 <div className="w-full md:w-1/2 pt-5 md:pt-0">
-                  <div className="aspect-h-9 aspect-w-16">
+                  <div className="aspect-h-8 aspect-w-16">
                     <div className="">
                       <GatsbyImage
                         className="w-auto max-w-full h-full"
